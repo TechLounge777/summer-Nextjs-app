6a336d5a2853a08908c84032a8ec58cc68272ba3
TechLounge777 <msblanche777@gmail.com>
2021-08-08 16:14:18 +0200

    added articles

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
 
 export default function Home({articles}) {
   return (
@@ -7,9 +8,8 @@ export default function Home({articles}) {
         <title>Summer Nextjs App</title>
         <meta name='keywords' content='web development, programming'></meta>
       </Head>
-     {articles.map((article) => (
-       <h3>{article.title}</h3>
-     ))}
+
+      <ArticleList articles={articles} />
     </div>
   )
 }

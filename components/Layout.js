ea24751dad43655d7cce2aa74cee564c4379f3f8
TechLounge777 <msblanche777@gmail.com>
2021-08-08 11:45:10 +0200

    added Nav.js

--- components/Layout.js
+++ components/Layout.js
@@ -2,12 +2,15 @@ import styles from '../styles/Layout.module.css'
 
 const Layout = ({children}) => {
     return (
-        <div className={styles.container}>
-            <main className={styles.main}>
-                {children}
-            </main>
-            
-        </div>
+        <>
+            <Nav />
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    {children}
+                </main>
+                
+            </div>
+        </>
     )
 }
 
